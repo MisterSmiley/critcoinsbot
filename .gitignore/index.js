37a5a5ea27a293036a959cc61e3dae7ef628bb61
MisterSmiley <35432693+MisterSmiley@users.noreply.github.com>
2018-01-14 21:44:52 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,11 +3,15 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function(){
-    bot.user.setGame("c!help | CritCoinsBot")
-    if (message.content === 'c!ping')
+    bot.user.setGame("c!help | CoinsBot");
+
+})
+
+bot.on('message', function(message) {
+    if (message.content === 'c!ping') {
       message.reply('Pong !')
     console.log("Le Bot est connecté")
 
 });
 
-bot.login("NDAyMTkyNDU5NTQ5MTE0Mzc4.DT1KcQ.atV3-7qA5bX5hJQtM_ogNlK6ehs");
+bot.login("NDAyMTkyNDU5NTQ5MTE0Mzc4.DT1SmQ.g7ffYVv48FCuqPpFgB7uJqAMtpA");
