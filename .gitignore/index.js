9de42afd4e20892d88684b84d68e37eea378bbc5
MisterSmiley <35432693+MisterSmiley@users.noreply.github.com>
2018-01-14 23:27:26 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,6 +5,7 @@ var bot = new Discord.Client();
 bot.on("ready", function(){
     bot.user.setGame("c!help | CoinsBot");
     console.log("Le Bot est connecté")
+});
 	
 bot.on('message', message => {
 	if (message.content === ("c!ping")){
@@ -20,8 +21,6 @@ bot.on('message', message => {
 	m += 'je suis présent dans ${client.guild.size} serveurs \n';
 	message.author.sendMessage(m).catch(console.log); 
 }
-});
-
 });
 
 bot.login("NDAyMTkyNDU5NTQ5MTE0Mzc4.DT1KcQ.atV3-7qA5bX5hJQtM_ogNlK6ehs");
