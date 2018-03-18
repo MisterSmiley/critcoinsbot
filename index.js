1790dd9cabebc3333e713630c4a82d6ceeddd61a
MisterSmiley <35432693+MisterSmiley@users.noreply.github.com>
2018-03-18 13:15:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 bot.on("ready", ready => {
-    bot.user.setGame("dev le SmileyBot")
+    bot.user.setGame("Michael Jackson - ")
+});
+
+bot.on('ready', () => {
+  console.log("AlphaBot is ready")
+  setInterval(function(){
+    guilds = ["Michael Jackson - Beat It", "Michael Jackson - Billie Jean", "Michael Jackson - Thriller Thriller"]
+    lecture = Math.floor((Math.random() * guilds.length));
+    bot.user.setPresence({
+      game:{
+        name: `${guilds[lecture]}`, 
+        type: 3
+      }
+    });
+  }, 60000);  
 });
 
 bot.login(process.env.TOKEN);
